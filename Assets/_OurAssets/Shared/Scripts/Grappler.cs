6a26a561bc1931eb73765c295513cf515d3dca0e
gregory-code <gregrigdon@icloud.com>
2024-04-13 11:22:04 -0500

    New anim
    nice

---
+++ Assets/_OurAssets/Shared/Scripts/Grappler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grappler : CharacterBase
+{
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
